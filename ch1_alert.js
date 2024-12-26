f19b281d1849cdbe2e3b7cf4f4b98a9ee61e554a
tahirmood <90515346+tahirmood@users.noreply.github.com>
2024-12-26 09:12:44 +0000

    Add files via upload

---
+++ ch1_alert.js
@@ -0,0 +1,9 @@
+let welc = "Hello, ";
+let nam = "my name is T Mahm,";
+let age = "I am 59 years old and ";
+let cod = "I can code JavaScript ";
+//let x = 1;
+let bool = true;
+let first = welc + nam + age;
+let secon = cod + bool;
+console.log(first, secon);
